28dac3b49c364a91a633167ad0e68e718bef8933
Danny <thanhan1100@gmail.com>
2024-09-21 00:24:08 +0700

    Add Booking feature with Ticket model and controller
    Updated project and solution files to include new dependencies and project entries. Added a new `BookingController` with CRUD actions and sample data. Created a `Ticket` model in the `AlpineSkiHouse.Models` namespace. Updated `_Layout.cshtml` to include a navigation link for booking tickets. Added an `Index.cshtml` view to display tickets in a table format.

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AlpineSkiHouse.Models;
+
+namespace AlpineSkiHouse.Controllers
+{
+    public class BookingController : Controller
+    {
+        // Create a data of type Ticket
+        static List<Ticket> tickets = new List<Ticket>
+        {
+            new Ticket { Id = 1, CustomerName = "Danny Vetiso", RoomNumber = "A102" , CheckIn = GetRandomDateTime(), CheckOut = GetRandomDateTime()},
+            new Ticket { Id = 2, CustomerName = "Log Rosswell", RoomNumber = "B202" , CheckIn = GetRandomDateTime(), CheckOut = GetRandomDateTime()},
+            new Ticket { Id = 3, CustomerName = "Tunic (The Fox)", RoomNumber = "C125" , CheckIn = GetRandomDateTime(), CheckOut = GetRandomDateTime()}
+        };
+
+        // Random Date Function
+        static DateTime GetRandomDateTime()
+        {
+            Random random = new Random();
+            DateTime start = DateTime.Now.AddMonths(-1); // Start date (1 month ago)
+            int range = (DateTime.Now - start).Days; // Range of days between start date and now
+            return start.AddDays(random.Next(range)); // Add a random number of days to the start date
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", tickets);
+        }
+
+        // Create
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // Read
+        public IActionResult Read()
+        {
+            return View("Read");
+        }
+
+        // Update
+        public IActionResult Update()
+        {
+            return View("Update");
+        }
+
+        // Remove
+        public IActionResult Remove()
+        {
+            return View("Remove");
+        }
+
+
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿namespace AlpineSkiHouse.Models
+{
+    public class Ticket
+    {
+        public required int Id { get; set; }
+        
+        public required string CustomerName { get; set; }
+        
+        public required string RoomNumber { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+    }
+}
